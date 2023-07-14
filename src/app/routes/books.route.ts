a006f211f342bd4d89f9741c41344894ac56e2ab
mahabubr <mahabub.r753@gmail.com>
2023-07-14 22:03:23 +0600

    book delete router  created

--- src/app/routes/books.route.ts
+++ src/app/routes/books.route.ts
@@ -5,6 +5,7 @@ const router = express.Router()
 
 router.get('/', BookController.getAllBooks)
 router.post('/', BookController.postBook)
-router.patch('/', BookController.updateBook)
+router.patch('/:id', BookController.updateBook)
+router.delete('/:id', BookController.deleteBook)
 
 export const BookRouter = router
