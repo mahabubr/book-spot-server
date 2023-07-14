4b8f5643ddc8df3ddf7965ecc302b84e4d4837ef
mahabubr <mahabub.r753@gmail.com>
2023-07-14 21:52:19 +0600

    update book router implement

--- src/app/routes/books.route.ts
+++ src/app/routes/books.route.ts
@@ -5,5 +5,6 @@ const router = express.Router()
 
 router.get('/', BookController.getAllBooks)
 router.post('/', BookController.postBook)
+router.patch('/', BookController.updateBook)
 
 export const BookRouter = router
