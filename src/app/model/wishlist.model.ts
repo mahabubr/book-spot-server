3343883e1508b3c8fb86e1129e56da32a135fc65
mahabubr <mahabub.r753@gmail.com>
2023-07-16 20:11:36 +0600

    wrap back end

--- src/app/model/wishlist.model.ts
+++ src/app/model/wishlist.model.ts
@@ -3,12 +3,12 @@ import { IWishlist, WishlistModel } from "../interface/wishlist.interface";
 
 const wishlistSchema = new Schema<IWishlist>(
   {
-    title: { type: String, required: true },
-    author: { type: String, required: true },
-    genre: { type: String, required: true },
-    publication_date: { type: String, required: true },
-    email: { type: String, required: true },
-    user_email: { type: String, required: true },
+    title: { type: String },
+    author: { type: String },
+    genre: { type: String },
+    publication_date: { type: String },
+    email: { type: String },
+    user_email: { type: String },
     reviews: { type: [] },
   },
   {
